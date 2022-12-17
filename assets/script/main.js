71b16a534a43f1f74599329ce97a544ed8ee2a25
moonlightlizaa <eliza.lacroes@gmail.com>
2022-12-17 16:45:32 +0100

    edits

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,87 +1,96 @@
+// 1 Interactie voor frontend: Vibrate - click
+
+let frontend = document.querySelector("a:nth-of-type(1)");
+frontend.addEventListener("click", vibrateHandler);
+frontend.addEventListener("animationend", vibrateHandler);
+function vibrateHandler() {
+  frontend.classList.toggle("vibrate");
+}
+
+// 2 Interactie voor design:  Jelly - click
+
+let design = document.querySelector("a:nth-of-type(2)");
+design.addEventListener("click", jellyHandler);
+design.addEventListener("animationend", jellyHandler);
+function jellyHandler() {
+  design.classList.toggle("jelly");
+}
+
+// 3 Interactie voor And :  Flip - click
+
+let and = document.querySelector("a:nth-of-type(3)");
+and.addEventListener("click", flipHandler);
+and.addEventListener("animationend", flipHandler);
+function flipHandler() {
+  and.classList.toggle("flip-1");
+}
+
+// 4 Interactie voor Development: Pulse - click
+
+let development = document.querySelector("a:nth-of-type(4)");
+development.addEventListener("click", pulseHandler);
+development.addEventListener("animationend", pulseHandler);
+function pulseHandler() {
+  development.classList.toggle("pulse");
+}
+
+// 5 Interactie voor Sprint 5: Blink - click
+
+let sprint5 = document.querySelector("a:nth-of-type(5)");
+sprint5.addEventListener("click", blinkHandler);
+sprint5.addEventListener("animationend", blinkHandler);
+function blinkHandler() {
+  sprint5.classList.toggle("blink");
+}
+
+// 6 Interactie voor Fix:
+
+let fix = document.querySelector("a:nth-of-type(6)");
+fix.addEventListener("click", shadowHandler);
+fix.addEventListener("animationend", shadowHandler);
+function shadowHandler() {
+  fix.classList.toggle("shadow-1");
+}
+
+// 7  Interactie voor The:
+
+// 8 Interactie voor Flow:
+
+// 9 Interactie voor User:
+
+// 10 Interactie voor Interface:
+
+// 11 Interactie voor Events:
+
+// 12 Interactie voor Interaction: Bounce - click
+
+let interaction = document.querySelector("a:nth-of-type(12)");
+interaction.addEventListener("click", bounceHandler);
+interaction.addEventListener("animationend", bounceHandler);
+function bounceHandler() {
+  interaction.classList.toggle("bounce");
+}
+
 // 0 color change
 
 const button = document.querySelectorAll("a");
 
 button.forEach((button) => {
-  // Single click event listener
   button.addEventListener("click", function () {
-    button.classList.add("green");
+    button.classList.add("blue");
   });
 
   button.addEventListener("dblclick", function () {
     button.classList.add("pink");
   });
+});
 
-  // 1 Interactie voor frontend: Vibrate - click
+// reset
 
-  let frontend = document.querySelector("a:nth-of-type(1)");
-  frontend.addEventListener("click", vibrateHandler);
-  frontend.addEventListener("animationend", vibrateHandler);
-  function vibrateHandler() {
-    frontend.classList.toggle("vibrate");
-  }
-
-  // 2 Interactie voor design:  Jelly - click
-
-  let design = document.querySelector("a:nth-of-type(2)");
-  design.addEventListener("click", jellyHandler);
-  design.addEventListener("animationend", jellyHandler);
-  function jellyHandler() {
-    design.classList.toggle("jelly");
-  }
-
-  // 3 Interactie voor And :  Flip - click
-
-  let and = document.querySelector("a:nth-of-type(3)");
-  and.addEventListener("click", flipHandler);
-  and.addEventListener("animationend", flipHandler);
-  function flipHandler() {
-    and.classList.toggle("flip-1");
-  }
-
-  // 4 Interactie voor Development: Pulse - click
-
-  let development = document.querySelector("a:nth-of-type(4)");
-  development.addEventListener("click", pulseHandler);
-  development.addEventListener("animationend", pulseHandler);
-  function pulseHandler() {
-    development.classList.toggle("pulse");
-  }
-
-  // 5 Interactie voor Sprint 5: Blink - click
-
-  let sprint5 = document.querySelector("a:nth-of-type(5)");
-  sprint5.addEventListener("click", blinkHandler);
-  sprint5.addEventListener("animationend", blinkHandler);
-  function blinkHandler() {
-    sprint5.classList.toggle("blink");
-  }
-
-  // 6 Interactie voor Fix:
-
-  let fix = document.querySelector("a:nth-of-type(6)");
-  fix.addEventListener("click", shadowHandler);
-  fix.addEventListener("animationend", shadowHandler);
-  function shadowHandler() {
-    fix.classList.toggle("shadow-1");
-  }
-
-  // 7  Interactie voor The:
-
-  // 8 Interactie voor Flow:
-
-  // 9 Interactie voor User:
-
-  // 10 Interactie voor Interface:
-
-  // 11 Interactie voor Events:
-
-  // 12 Interactie voor Interaction: Bounce - click
-
-  let interaction = document.querySelector("a:nth-of-type(12)");
-  interaction.addEventListener("click", bounceHandler);
-  interaction.addEventListener("animationend", bounceHandler);
-  function bounceHandler() {
-    interaction.classList.toggle("bounce");
-  }
+button.forEach((button) => {
+  button.addEventListener("keydown", function (event) {
+    if (event.key === "a") {
+      button.classList.add("reset");
+    }
+  });
 });
